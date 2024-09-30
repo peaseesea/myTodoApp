765bc15174f5646a77751a73ac0f6a23c0e616a7
pches <p.chesterman@outlook.com>
2024-09-30 14:26:02 +0100

    correct missing text_input variable

--- web.py
+++ web.py
@@ -22,7 +22,7 @@ for index, todo in enumerate(todos):
         st.rerun()
 
 
-st.text_input(placeholder="Add new todo....",
+st.text_input(label=" ",placeholder="Add new todo....",
               on_change=add_todo, key='new_todo')
 
-st.session_state
+
